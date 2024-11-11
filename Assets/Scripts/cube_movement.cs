20640992788b696f6a424fedb2665dbdcc46fb24
PatrickMonster <patrykbudniok@gmail.com>
2024-11-11 22:32:59 +0100

    First Commit

---
+++ Assets/Scripts/cube_movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class cube_movement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetAxis("Horizontal") > 0) 
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.AddForce(-Vector3.right * speed);
+                
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rb.AddForce(-Vector3.forward * speed);
+
+        }
+
+
+
+
+    }
+
+
+}
