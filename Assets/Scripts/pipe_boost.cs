d58abafcd50003c46123767a5869fa142c59706a
PatrickMonster <patrykbudniok@gmail.com>
2024-11-25 19:52:59 +0100

    25.11.24

---
+++ Assets/Scripts/pipe_boost.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class pipe_boost : MonoBehaviour
+{
+    public cube_movement kula;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   private void OnTriggerEnter(Collider collision)
+   {
+    if(collision.gameObject.CompareTag("Kula"))
+    {
+        kula.rb.AddForce(Vector3.up * 75, ForceMode.Impulse);
+    }
+   }
+}
--- Assets/Scripts/platform_staying.cs
+++ Assets/Scripts/platform_staying.cs
@@ -1,20 +1,29 @@
+using NUnit.Framework.Constraints;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class platform_staying : MonoBehaviour
 {
+    public cube_movement kulka;
+    void Start()
+    {
+       
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Kula")
         {
             Debug.Log("DZIALA XD");
             other.transform.parent = transform;
+            //kulka.rb.constraints = RigidbodyConstraints.FreezePositionZ;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag == "Kula")
+        if (other.gameObject.tag == "Kula")
         {
-            other.transform.parent = null;
+             other.transform.parent = null;
+            //kulka.rb.constraints = RigidbodyConstraints.None;
         }
     }
 
