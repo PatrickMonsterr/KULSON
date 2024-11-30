fcddaca73ced3e00536b0b9c5eefa000909519dc
PatrickMonster <patrykbudniok@gmail.com>
2024-11-30 18:27:00 +0100

    30.11.24

--- Assets/Scripts/Leaf_jump_real.cs
+++ Assets/Scripts/Leaf_jump_real.cs
@@ -4,24 +4,47 @@ public class Leaf_jump_real : MonoBehaviour
 {
 
     public cube_movement kula_move;
+    public Animator bump;
     private float odbicie = 20f;
-    
-    private void OnCollisionEnter(Collision collision)
+
+
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Kula"))
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            bump.SetTrigger("Bump");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Kula"))
+        {
+            Debug.Log("przed triggerem");
+            bump.SetTrigger("Bump");
+
+
+
+
+        }
+
+
+
+
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Kula"))
         {
             kula_move.rb.AddForce(Vector3.up * odbicie, ForceMode.Impulse);
-            
 
-            if(odbicie < 120)
+
+            if (odbicie < 40)
             {
                 odbicie += 20f;
             }
             Debug.Log(odbicie);
         }
-        
-        
-
     }
     public void ResetOdbicie()
     {
